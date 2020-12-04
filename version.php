4c791c900893f4cd51cd8e9a7e109d2d5ac14f5a
Айнур Шарипов <aasharipov@edu.hse.ru>
2020-12-04 13:31:33 +0300

    Dual version support

--- version.php
+++ version.php
@@ -2,11 +2,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2020041452;
+$plugin->version  = 2020120100;
 $plugin->requires = 2018050800;
 $plugin->cron = 0;
 $plugin->component = 'qbehaviour_interactive_for_digitalliteracy';
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = '0.1.0';
+$plugin->release = '0.2.0';
 
 $plugin->dependencies = array();
